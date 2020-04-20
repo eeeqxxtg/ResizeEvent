9b962277d4191824621d81b75ffa451c0f5f3fe9
John Wang <johnwang20150803@gmail.com>
2020-04-20 17:51:20 +0800

    Update and rename type.ts to utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+export type IResizeCallBack = () => void;
+
+export const debounce = (fn: (evt: any) => void, delay = 60) => {
+  let timer: any = null;
+  return (...rest: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(rest);
+    }, delay);
+  };
+};
